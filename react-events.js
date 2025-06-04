0a00d46c9977c9f0d1803d48fa5c8be1e715a5ef
Jeevan Kumar ( G1 ) <114385490+jeevan42@users.noreply.github.com>
2025-06-04 22:40:12 +0530

    Create react-events.js

---
+++ react-events.js
@@ -0,0 +1,10 @@
+/*
+📘 Handling Events:
+React handles events similarly to DOM, but with camelCase.
+
+<button onClick={handleClick}>Click</button>
+
+function handleClick() {
+  console.log('Clicked');
+}
+*/
