187e9cd3fbf8f0c35b9f9d4ddf50a65503a6fcac
Jeevan Kumar ( G1 ) <114385490+jeevan42@users.noreply.github.com>
2025-06-04 22:39:11 +0530

    Create react-conditional-rendering.js

---
+++ react-conditional-rendering.js
@@ -0,0 +1,12 @@
+/*
+📘 Conditional Rendering:
+React lets you render elements based on conditions.
+
+Methods:
+- if/else
+- ternary (condition ? A : B)
+- && operator
+
+Example:
+{isLoggedIn ? <Dashboard /> : <Login />}
+*/
