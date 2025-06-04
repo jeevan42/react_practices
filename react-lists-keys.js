b55c71d4da6948a45a66d6a6ac3fe2e81038fff1
Jeevan Kumar ( G1 ) <114385490+jeevan42@users.noreply.github.com>
2025-06-04 22:39:46 +0530

    Create react-lists-keys.js

---
+++ react-lists-keys.js
@@ -0,0 +1,9 @@
+/*
+📘 Rendering Lists & Keys:
+We can use map() to render a list of elements.
+Each child in a list should have a unique "key" prop.
+
+Example:
+const items = ['a', 'b'];
+{items.map((item, i) => <li key={i}>{item}</li>)}
+*/
