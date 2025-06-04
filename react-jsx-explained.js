25300626ab6d3c4d5f09e726e84fa890f2ee9380
Jeevan Kumar ( G1 ) <114385490+jeevan42@users.noreply.github.com>
2025-06-04 22:36:03 +0530

    Create react-jsx-explained.js

---
+++ react-jsx-explained.js
@@ -0,0 +1,13 @@
+/*
+📘 JSX (JavaScript XML) - Definition:
+JSX is a syntax extension for JavaScript used with React.
+It allows writing HTML-like code inside JavaScript, making component structure clear and concise.
+
+JSX is not required, but it helps build UIs intuitively.
+
+Example:
+const element = <h1>Hello, world!</h1>;
+
+JSX gets compiled to React.createElement():
+const element = React.createElement('h1', null, 'Hello, world!');
+*/
