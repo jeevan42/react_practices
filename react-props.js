5f88f5d002cab85dbfa55a7f9c82726a0570ab83
Jeevan Kumar ( G1 ) <114385490+jeevan42@users.noreply.github.com>
2025-06-04 22:37:14 +0530

    Create react-props.js

---
+++ react-props.js
@@ -0,0 +1,13 @@
+/*
+📘 Props in React:
+Props (short for properties) are read-only inputs passed to components.
+They make components reusable by allowing dynamic content.
+
+Example:
+<Greet name="Jeevan" />
+
+In component:
+function Greet(props) {
+  return <h1>Hello, {props.name}</h1>;
+}
+*/
