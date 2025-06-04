cabc0992da6eee5782768fb432cd680ccfba9161
Jeevan Kumar ( G1 ) <114385490+jeevan42@users.noreply.github.com>
2025-06-04 22:38:42 +0530

    Create react-useEffect.js

---
+++ react-useEffect.js
@@ -0,0 +1,11 @@
+/*
+📘 useEffect Hook:
+useEffect runs after every render by default.
+
+useEffect(() => {
+  console.log("Component rendered");
+}, [dependencies]);
+
+- [] => runs once (on mount)
+- [state] => runs when `state` changes
+*/
