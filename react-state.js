96895d698e0ee2dc04e8e905c606379db94619aa
Jeevan Kumar ( G1 ) <114385490+jeevan42@users.noreply.github.com>
2025-06-04 22:37:37 +0530

    Create react-state.js

---
+++ react-state.js
@@ -0,0 +1,10 @@
+/*
+📘 State in React:
+State is a built-in object used to contain data or information about the component.
+Changing state re-renders the component.
+
+useState() hook is used in functional components:
+
+const [count, setCount] = useState(0);
+setCount(count + 1);
+*/
