b32834bd9c058bd17f8e498b658ba57bfe65a597
Jeevan Kumar ( G1 ) <114385490+jeevan42@users.noreply.github.com>
2025-06-04 22:36:37 +0530

    Create react-components.js

---
+++ react-components.js
@@ -0,0 +1,15 @@
+/*
+📘 React Components - Definition:
+Components are reusable, independent pieces of UI in React.
+
+🛠️ Types:
+- Functional Components: Simple JS functions
+- Class Components: Older way using ES6 classes (less common today)
+
+Example (Functional):
+function Greet() {
+  return <h1>Hello</h1>;
+}
+
+Components can receive props and manage state.
+*/
