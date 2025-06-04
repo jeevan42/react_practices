f5d763976ceecd507b4a4c3426efaf23a6fdaa87
Jeevan Kumar ( G1 ) <114385490+jeevan42@users.noreply.github.com>
2025-06-04 22:38:16 +0530

    Create react-lifecycle.js

---
+++ react-lifecycle.js
@@ -0,0 +1,8 @@
+/*
+📘 React Lifecycle (Class components):
+- Mounting: constructor → render → componentDidMount
+- Updating: shouldComponentUpdate → render → componentDidUpdate
+- Unmounting: componentWillUnmount
+
+With functional components, we use useEffect() to mimic lifecycle behavior.
+*/
